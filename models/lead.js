515f79bbb12bf59f4da45dab102a2d117eff8af9
Brendenw <brendenw@hotmail.com>
2016-05-15 19:32:58 +0000

    adding local db connection example

--- models/lead.js
+++ models/lead.js
@@ -1,17 +1,15 @@
 var credentials = require("../lib/credentials");
 var mongoose = require("mongoose");
 
-var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
-                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };       
- 
-mongoose.connect(credentials.mongo.development.connectionString, options);
+// remote db settings 
+ var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 }  } };       
+ mongoose.connect(credentials.mongo.development.connectionString, options);
 
-// settings for connecting to db on local machine
+// local db settings 
 // var ip = process.env.ip || '127.0.0.1';
 // mongoose.connect('mongodb://' +ip+ '/projects');
 
 var conn = mongoose.connection; 
- 
 conn.on('error', console.error.bind(console, 'connection error:'));  
 
 var leadSchema = mongoose.Schema({

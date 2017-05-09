31828e292d52ebf2ca275a91205331d90ca921d6
Brendenw <brendenw@hotmail.com>
2017-05-09 16:17:16 +0000

    adding test cases

--- test/book.js
+++ test/book.js
@@ -1,15 +1,35 @@
-var expect = require("chai").expect;
-var book = require("../lib/book");
+'use strict'
+const expect = require("chai").expect;
+const book = require("../lib/book");
 
 describe("Book", function() {
-    it("get returns requested book", function() {
-        var result = book.get("dune");
+    
+    it("returns requested book", function() {
+        let result = book.get("dune");
         expect(result).to.deep.equal({title: "dune", author:"frank herbert", pubdate:1969});
     });
     
-    it("get fails w/ invalid book", function() {
-        var result = book.get("fake");
+    it("fails to return an w/ invalid book", function() {
+        let result = book.get("fake");
         expect(result).to.be.undefined;
+    });
+
+    it("adds a new book", function() {
+        let result = book.add({title: "dune emperor", author:"frank herbert", pubdate:1969});
+        expect(result.added).to.be.true;
+    });
+    it("fails to add an existing book", function() {
+        let result = book.add({title: "dune", author:"frank herbert", pubdate:1969});
+        expect(result.added).to.be.false;
+    });
+
+    it("deletes an existing book", function() {
+        let result = book.delete("dune");
+        expect(result.deleted).to.be.true;
+    });
+    it("fails to delete an invalid book", function() {
+        let result = book.delete("travels with charlie");
+        expect(result.deleted).to.be.false;
     });
 
 });

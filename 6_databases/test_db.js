899d2c956d078e946eef24894e787e90b0b1f612
Brendenw <brendenw@hotmail.com>
2017-05-20 22:27:50 +0000

    update test_db.js example

--- 6_databases/test_db.js
+++ 6_databases/test_db.js
@@ -1,14 +1,14 @@
 var Book = require("../models/book");
 
 // insert a new document into the database
-new Book({title:"dune", author:"frank herbert", amount: 10, pubdate: "05-21-1969"}).save();
+//new Book({title:"dune", author:"frank herbert", amount: 10, pubdate: "05-21-1969"}).save();
 
 Book.count((err, result)=>{
-    console.log(result)
+    console.log(result);
 });
 
 // find all documents 
-Book.find(function(err, result) {
+Book.find((err, result) => {
     // output error if one occurred
     if (err) {
         console.log(err);

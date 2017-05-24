5ff4adc6d3b4fcb30d9d14a277f8b50a37ceb54a
Brendenw <brendenw@hotmail.com>
2017-05-24 04:39:41 +0000

    add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+     "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module"
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "prefer-arrow-callback": "warn",
+        "no-var": "warn"
+    }
+};

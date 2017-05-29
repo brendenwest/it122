ed850086672713c152fade2dad915d4b4fa65a16
Brendenw <brendenw@hotmail.com>
2017-05-29 05:21:05 +0000

    react sample w/o API integration

---
+++ 7_react/index.js
@@ -0,0 +1,75 @@
+'use strict'
+
+var express = require("express");
+var app = express();
+var Book = require("../models/book"); // use database model
+
+// configure Express app
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/../public'));
+app.use(require("body-parser").urlencoded({extended: true}));
+app.use((err, req, res, next) => {
+  console.log(err)
+})
+
+// set template engine
+let handlebars =  require("express-handlebars");
+app.engine(".html", handlebars({extname: '.html', defaultLayout: 'main' }));
+app.set("view engine", ".html");
+
+app.get('/', (req,res) => {
+    Book.find((err,books) => {
+        if (err) return next(err);
+        res.render('home', {books: JSON.stringify(books)});    
+    })
+});
+
+app.get('/about', (req,res) => {
+    res.type('text/html');
+    res.render('about');
+});
+
+// api's
+app.get('/api/v1/book/:title', (req, res, next) => {
+    let title = req.params.title;
+    console.log(title);
+    Book.findOne({title: title}, (err, result) => {
+        if (err || !result) return next(err);
+        res.json( result );    
+    });
+});
+
+app.get('/api/v1/books', (req,res, next) => {
+    Book.find((err,results) => {
+        if (err || !results) return next(err);
+        res.json(results);
+    });
+});
+
+app.get('/api/v1/delete/:title', (req,res, next) => {
+    Book.remove({"title":req.params.title }, (err, result) => {
+        if (err) return next(err);
+        // return # of items deleted
+        res.json({"deleted": result.result.n});
+    });
+});
+
+app.get('/api/v1/add/:title/:author/:pubdate', (req,res, next) => {
+    // find & update existing item, or add new 
+    let title = req.params.title;
+    Book.update({ title: title}, {title:title, author: req.params.author, pubdate: req.params.pubdate }, {upsert: true }, (err, result) => {
+        if (err) return next(err);
+        // nModified = 0 for new item, = 1+ for updated item 
+        res.json({updated: result.nModified});
+    });
+});
+
+app.use((req,res) => {
+    res.type('text/plain'); 
+    res.status(404);
+    res.send('404 - Not found');
+});
+
+app.listen(app.get('port'), () => {
+    console.log('Express started');    
+});
